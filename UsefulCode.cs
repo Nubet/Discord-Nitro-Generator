56a7440e98c5def4763d97ccd537f133d493cf2f
Nubet <77124888+Nubet@users.noreply.github.com>
2021-03-31 13:43:10 +0200

    Add files via upload

---
+++ UsefulCode.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Data;
+using System.Collections.Generic;
+
+namespace Name
+{
+    class Program
+    {
+        public static string Nitro;
+
+        static void Main(string[] args)
+
+       
+        {
+                            int lenght = 16;
+                            var range = "123AbCcDdEeFfGgHhIiJjKkLlMm789NnOoPpQqRrSsTt0UuVvWwXxYyZz456";
+                            var GenerujRandomowyString = new Random();
+                            Nitro = "https://discord.gift/"+ new string(
+                            Enumerable.Repeat(range, lenght)
+                           .Select(s => s[GenerujRandomowyString.Next(s.Length)])
+                           .ToArray());
+                          
+        }
+                             
+        
+    }
+}
+                                                                            
