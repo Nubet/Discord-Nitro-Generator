bd7be98633f2b1ca338e1662bfa50d558962a90a
Nubet <77124888+Nubet@users.noreply.github.com>
2022-01-15 14:56:20 +0100

    Update UsefulCode.cs

--- UsefulCode.cs
+++ UsefulCode.cs
@@ -1,32 +1,12 @@
-using System;
-using System.IO;
-using System.Text;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Diagnostics;
-using System.Threading.Tasks;
-using System.Data;
-using System.Collections.Generic;
 
-namespace Name
-{
-    class Program
-    {
-        public static string Nitro;
+int lenght = 16; // lenght of random string
+var range = "123AbCcDdEeFfGgHhIiJjKkLlMm789NnOoPpQqRrSsTt0UuVvWwXxYyZz456";
+var GenerujRandomowyString = new Random();
+string Nitro;
 
-        static void Main(string[] args)
-        {
-             int lenght = 16;
-             var range = "123AbCcDdEeFfGgHhIiJjKkLlMm789NnOoPpQqRrSsTt0UuVvWwXxYyZz456";
-             var GenerujRandomowyString = new Random();
-             string Nitro;
+Nitro = "https://discord.gift/"+ new string(
+Enumerable.Repeat(range, lenght)
+Select(s => s[GenerujRandomowyString.Next(s.Length)])
+.ToArray());
 
-             Nitro = "https://discord.gift/"+ new string(
-             Enumerable.Repeat(range, lenght)
-             Select(s => s[GenerujRandomowyString.Next(s.Length)])
-            .ToArray());
-        }
-    
-    }
-}
-    //a piece of my code                                                                        
+//a piece of my code                                                                        
